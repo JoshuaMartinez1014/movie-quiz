3fe468777b0aabee32eceba75ccc23d6839b45c1
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-04-08 19:17:33 -0500

    functional-movie-quiz-website

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let questionContainer = document.querySelector("#card-questions");
 let questionTitle = document.querySelector(".card-title");
 let submitBtn = document.querySelector("#submit-button");
 let mainCard = document.querySelector(".main-card");
+let scoreBoardBtn = document.querySelector("#scoreBoard");
+let buttonContainer = document.querySelector(".buttons");
 
 let timer = document.querySelector(".timer");
 let i = 0;
@@ -15,91 +17,91 @@ let score = [];
 /* Question Objects */
 const questionSet = [
   {
-    title: "question1",
+    title: "When did the original, David Lynch, Dune come out",
     answers: [
       {
-        text: "q1 answer1",
+        text: "1999",
+        isCorrect: false,
+      },
+      {
+        text: "1984",
         isCorrect: true,
       },
       {
-        text: "q1 answer2",
+        text: "1987",
         isCorrect: false,
       },
       {
-        text: "q1 answer3",
-        isCorrect: false,
-      },
-      {
-        text: "q1 answer4",
+        text: "2001",
         isCorrect: false,
       },
     ],
   },
   {
-    title: "question2",
+    title: "Which pill did Neo take in the Matrix",
     answers: [
       {
-        text: "q2 answer1",
+        text: "red pill",
         isCorrect: true,
       },
       {
-        text: "q2 answer2",
+        text: "blue pill",
         isCorrect: false,
       },
     ],
   },
   {
-    title: "question3",
+    title: "Which actor played themselves in ZombieLand",
     answers: [
       {
-        text: "answer1",
+        text: "Woody Harrelson",
+        isCorrect: false,
+      },
+      {
+        text: "Jennifer Lawrence",
+        isCorrect: false,
+      },
+      {
+        text: "Ryan Gosling",
+        isCorrect: false,
+      },
+      {
+        text: "Bill Murry",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    title: "Is Darth-Vador Lukes Father",
+    answers: [
+      {
+        text: "true",
         isCorrect: true,
       },
       {
-        text: "answer2",
-        isCorrect: false,
-      },
-      {
-        text: "answer3",
-        isCorrect: false,
-      },
-      {
-        text: "answer4",
+        text: "false",
         isCorrect: false,
       },
     ],
   },
   {
-    title: "question4",
+    title: "How many takes did it take for the opening scene of LaLa Land",
     answers: [
       {
-        text: "q4 answer1",
+        text: "25",
+        isCorrect: false,
+      },
+      {
+        text: "32",
+        isCorrect: false,
+      },
+      {
+        text: "12",
+        isCorrect: false,
+      },
+      {
+        text: "47",
         isCorrect: true,
-      },
-      {
-        text: "q4 answer2",
-        isCorrect: false,
-      },
-    ],
-  },
-  {
-    title: "question5",
-    answers: [
-      {
-        text: "q5 answer1",
-        isCorrect: true,
-      },
-      {
-        text: "q5 answer2",
-        isCorrect: false,
-      },
-      {
-        text: "q5 answer3",
-        isCorrect: false,
-      },
-      {
-        text: "q5 answer4",
-        isCorrect: false,
       },
     ],
   },
@@ -111,6 +113,7 @@ function createQuestion() {
   console.log("i: " + i);
   // create questions and give them attributes
   if (i < questionSet.length) {
+    console.log(points);
     questionTitle.textContent = questionSet[i].title;
     for (let j = 0; j < questionSet[i].answers.length; j++) {
       let div = document.createElement("div");
@@ -122,9 +125,10 @@ function createQuestion() {
     // questions are done
   } else {
     displayScore();
+    scoreBoardBtn.setAttribute("style", "display: inline");
     console.log("questions are done");
     displayBtn();
-    score = 0;
+    /* score = 0; */
     clearInterval(currentInterval);
     i = -1;
   }
@@ -136,7 +140,7 @@ function clearBoard() {
   // hide scoreboard
   let scoreboard = document.querySelectorAll(".score");
   for (let j = 0; j < scoreboard.length; j++) {
-    scoreboard[j].setAttribute("style", "display: none;");
+    scoreboard[j].remove();
   }
   // hide previous questions
   let deleteQuestions = document.querySelectorAll(".questions");
@@ -155,24 +159,26 @@ function displayScore() {
   }
 
   if (localStorage.getItem("score") != null) {
-    console.log(points);
-    console.log("true");
-
     parseScore = JSON.parse(localStorage.getItem("score"));
     console.log(parseScore);
 
     newItem = { name: playerName, score: points };
     parseScore.push(newItem);
     localStorage.setItem("score", JSON.stringify(parseScore));
+    points = 0;
   } else {
-    console.log("null");
-    score.push({ name: playerName, score: points });
+    console.log(score);
+    score = [{ name: playerName, score: points }];
 
     localStorage.setItem("score", JSON.stringify(score));
+
     parseScore = score;
+    points = 0;
   }
 
   /*  let parseScore = JSON.parse(localStorage.getItem("score")); */
+
+  questionContainer.textContent = "";
 
   for (let j = 0; j < parseScore.length; j++) {
     questionTitle.textContent = "Scoreboard";
@@ -198,10 +204,14 @@ function showScoreBoard() {
 // display start button and reset variables
 function displayBtn() {
   submitBtn.setAttribute("style", "visibility: visible;");
+  scoreBoardBtn.setAttribute("style", "display: inline");
 }
 function endQuiz() {
+  i = 0;
+  clearInterval(currentInterval);
   clearBoard();
   displayBtn();
+
   displayScore();
   questionTitle.textContent = "You Fail!";
 }
@@ -221,28 +231,40 @@ function Starttimer() {
 
 function keepScore(id) {
   if (questionSet[i - 1].answers[id].isCorrect) {
-    console.log("is correct");
     points++;
   } else {
-    timer.textContent -= 15;
-    console.log("is wrong");
-    if (points >= 1) {
+    timer.textContent -= 10;
+
+    /* if (points >= 1) {
       points -= 1;
-    }
+    } */
   }
 }
 
-submitBtn.addEventListener("click", function (event) {
-  console.log(event);
-  timer.textContent = 60;
-  if (timer.textContent == 60) {
-    Starttimer();
+buttonContainer.addEventListener("click", function (event) {
+  if (submitBtn === event.target) {
+    timer.textContent = 60;
+    if (timer.textContent == 60) {
+      Starttimer();
+    }
+    playerName = prompt("enter player's name");
+    while (!playerName) {
+      playerName = prompt("enter player's name");
+    }
+    createQuestion();
+    scoreBoardBtn.setAttribute("style", "display: none");
+    submitBtn.setAttribute("style", "visibility: hidden;");
+    mainCard.setAttribute("style", "visibility: visible;");
+    playerNumber++;
   }
-  playerName = prompt("enter player's name");
-  createQuestion();
-  submitBtn.setAttribute("style", "visibility: hidden;");
-  mainCard.setAttribute("style", "visibility: visible;");
-  playerNumber++;
+  if (scoreBoardBtn === event.target) {
+    localStorage.clear();
+    clearBoard();
+    score = [""];
+    parseScore = [""];
+    console.log("score is:");
+    console.log(parseScore);
+  }
 });
 
 questionContainer.addEventListener("click", function (event) {
